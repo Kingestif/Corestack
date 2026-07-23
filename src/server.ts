4fcd8db540cfbb6da147e7e46241935fdcb19ccf
Kingestif <kingestiff@gmail.com>
2026-07-23 11:33:26 +0300

    feat: add app and server files

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import * as dotenv from "dotenv"
+import app from "./app.js"
+
+dotenv.config()
+
+const port = process.env.PORT
+
+app.listen(port, ()=> {
+    console.log("Server started listening")
+})
