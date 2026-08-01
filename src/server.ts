f4f2cca8838ba85342035f6526e7030334969a98
Kingestif <kingestiff@gmail.com>
2026-08-02 02:05:15 +0300

    feat: add zod validation

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,7 @@
-import * as dotenv from 'dotenv'
 import app from './app.js'
+import { ENV } from './env.js'
 
-dotenv.config()
-
-const port = process.env.PORT
+const port = Number(ENV.PORT)
 
 app.listen(port, () => {
     console.log('Server started listening')
