51321beb009342bac2e2eeaef6d62d76b820fa93
Kingestif <kingestiff@gmail.com>
2026-08-14 01:34:42 +0300

    feat: add zod error handling

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,5 +1,6 @@
 import type { ErrorRequestHandler } from 'express'
 import { AppError } from '../utils/errors/appError.js'
+import z from 'zod'
 
 export const errorHandler: ErrorRequestHandler = (error, _req, res, _next) => {
     let message = 'Internal server error'
@@ -11,6 +12,16 @@ export const errorHandler: ErrorRequestHandler = (error, _req, res, _next) => {
             message,
         })
     }
+
+    if (error instanceof z.ZodError) {
+        return res.status(400).json({
+            success: false,
+            code: 'VALIDATION_ERROR',
+            message: 'Invalid request data',
+            details: error.issues,
+        })
+    }
+
     return res.status(500).json({
         success: false,
         code: 'INTERNAL_SERVER_ERROR',
