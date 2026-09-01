e2028b53b8c0fef6ecb88ce90b2bdcadb200b967
Estif <kingestiff@gmail.com>
2026-09-01 04:38:10 +0300

    feat: add  json & html form parsers

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,8 @@ const app = express()
 
 app.use(helmet())
 app.use(compression())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
 app.use(limiter)
 
