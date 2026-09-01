7ee1e33bc4879a3f9942620f043e843a54b5fd11
Estif <kingestiff@gmail.com>
2026-09-01 05:15:07 +0300

    refactor: remove typecast as it handled on zod

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from './app.js'
 import { ENV } from './config/env.js'
 
-const port = Number(ENV.PORT)
+const port = ENV.PORT
 
 app.listen(port, () => {
     console.log('Server started listening')
